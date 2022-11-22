072ede6109abb1fe8151235f628cbe414aa566b0
Filip <dziubekfilip@gmail.com>
2022-11-22 18:17:06 +0100

    Add project files.

---
+++ Data/MovieLoverContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class MovieLoverContext : DbContext
+{
+    public string DbPath { get; }
+
+    public MovieLoverContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "MovieLoverDB.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
